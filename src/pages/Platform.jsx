e03eab3f6434fcc5ae510597bef8ed59b655b92e
quanhoangindex <quanhoangindex@gmail.com>
2026-05-04 15:07:14 +0200

    add: features sectio ui

--- src/pages/Platform.jsx
+++ src/pages/Platform.jsx
@@ -1,10 +1,12 @@
 import PlatformHero from "../components/sections/PlatformHero";
 import PlatformStats from "../components/sections/PlatformStats";
+import Features from "../components/sections/Features";
 function Platform() {
     return (
         <main>
             <PlatformHero />
             <PlatformStats />
+            <Features />
         </main>
     );
 }
