c4f8256f769879d8b1e22e0d54336bb583a98d21
quanhoangindex <quanhoangindex@gmail.com>
2026-05-10 17:52:45 +0200

    feat: add calendar library ui

--- src/components/sections/ContactSchedule.jsx
+++ src/components/sections/ContactSchedule.jsx
@@ -1,26 +1,55 @@
-import "./ContactSchedule.css"
-import Cal, { getCalApi } from "@calcom/embed-react"
-import { useEffect } from "react"
-import { ArrowUpRight } from "lucide-react"
+import "./ContactSchedule.css";
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+import { ArrowUpRight } from "lucide-react";
 
 function ContactSchedule() {
-
+    {
+        /*cal.com embed useEffect */
+    }
+    useEffect(() => {
+        (async function () {
+            const cal = await getCalApi({ namespace: "20min" });
+            cal("ui", {
+                theme: "light",
+                cssVarsPerTheme: { light: { "cal-brand": "#2563EB" } },
+                hideEventTypeDetails: false,
+                layout: "month_view",
+            });
+        })();
+    }, []);
 
     return (
         <section className="csched">
             <div className="csched__inner">
-
                 <div className="csched__title">
                     <p className="csched__label">
-                        FIND IT INTERESTING?<br />
+                        FIND IT INTERESTING?
+                        <br />
                         BOOK A CALL WITH US
                     </p>
                     <ArrowUpRight size={20} color="var(--text-secondary)" />
                 </div>
-
+                <div className="csched__cal">
+                    {/*cal.com embed */}
+                    <Cal
+                        namespace="20min"
+                        calLink="quan-hoang-ic6ech/20min"
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            overflow: "scroll",
+                        }}
+                        config={{
+                            layout: "month_view",
+                            useSlotsViewOnSmallScreen: "true",
+                            theme: "light",
+                        }}
+                    />
+                </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default ContactSchedule
+export default ContactSchedule;
